2d9ee8942947b5ae3a0e0aca30ef0ee3afbb2220
Avdhoot <avdhootu.27@gmail.com>
2021-12-20 16:32:15 +0530

    Camera

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+# webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# hand detector
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+# loop
+while True:
+    success, img = cap.read()
+
+
+    cv2.imshow("Camera", img)
+    cv2.waitKey(1)
