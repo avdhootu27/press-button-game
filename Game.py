dbc2e633a24a946b6a698f931c3544d190d63e25
Avdhoot <avdhootu.27@gmail.com>
2021-12-20 17:54:01 +0530

    Created target

--- Game.py
+++ Game.py
@@ -17,6 +17,10 @@ x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
 y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
 coeff = np.polyfit(x, y, 2)
 
+# game variables
+cx, cy = 250, 250
+color = (255, 0, 255)
+
 # loop
 while True:
     success, img = cap.read()
@@ -36,6 +40,11 @@ while True:
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,255), 5)
         cvzone.putTextRect(img, f'{int(distance_in_cm)} cm', (x+10, y+10))
 
+    # draw button
+    cv2.circle(img, (cx, cy), 20, color, cv2.FILLED)
+    cv2.circle(img, (cx, cy), 6, (255, 255, 255), cv2.FILLED)
+    cv2.circle(img, (cx, cy), 15, (255, 255, 255), 2)
+    cv2.circle(img, (cx, cy), 20, (0, 0, 0), 3)
 
     cv2.imshow("Camera", img)
     cv2.waitKey(1)
